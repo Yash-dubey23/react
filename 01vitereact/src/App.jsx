3afe4635462fd3f0bf3ba04bca2b9b3b0af909db
Yash Dubey <yashdubey9764@gmail.com>
2026-06-23 10:59:34 +0000

    basic

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -3,10 +3,11 @@ import Chai from "./chai"
 function App() {
 
 
+  const username = "chai aur code"
   return (
     <>
    <Chai />
-   <h1>GEt ready</h1>
+   <h1>GEt ready     {username}</h1>
    <p> You Know ... BUt what you know</p>
     </>
   )
---
+++ 02counter/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+    const [counter, setCounter]  = useState(15)
+
+  //let counter = 15
+
+  const addValue = () => {
+    if(counter>=20){
+      return;
+    }
+    //counter = counter + 1
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1 )
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    
+  }
+
+  const removeValue = () => {
+    if(counter===0){
+      return;
+    }
+    setCounter(counter - 1)
+  }
+  
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add value {counter}</button> 
+      <br />
+      <button
+      onClick={removeValue}
+      >remove value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
